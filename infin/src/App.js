a4b4b4c6156fb5ff4508bcc891de0f99c9460e03
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-06-28 16:29:40 -0400

    added header and background image

--- infin/src/App.js
+++ infin/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <Link to="/">
+            <img id="logo" src="https://vignette.wikia.nocookie.net/logocomics/images/d/d5/Infinity_Gauntlet_%281991%29.png/revision/latest?cb=20150519180606" alt="inflogo" />
+          </Link>
+        </div>
       </header>
     </div>
   );
