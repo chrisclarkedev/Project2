fa50a8a0c97d52e86105a8cac729734bb93e5fbd
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-06-29 23:15:15 -0400

    added hero-gallery

--- infin/src/components/Home.jsx
+++ infin/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ThanosSection from './ThanosSection';
+import PicSection from './PicSection';
 
 export default function Home() {
   return (
     <>
       <p class="speech">COME AND GET ME!</p>
       <ThanosSection />
+      <PicSection />
     </>
   )
 }
---
+++ infin/src/components/PicSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export default function ThanosSection() {
+  return (
+    <section id="pic-section">
+      <h2>Heroes</h2>
+      <div id="hero-gallery">
+
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/274.jpg" alt="capt-amer" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/140.jpg" alt="thor" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/133.jpg" alt="spdr-man" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/444.jpg" alt="scarl-wit" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/55.jpg" alt="dr_strange" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/83.jpg" alt="hulk" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/154.jpg" alt="adam-warlock" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/161.jpg" alt="wolv" />
+        <img src="https://www.superherodb.com/pictures2/portraits/10/100/85.jpg" alt="iron-man" />
+      </div>
+
+    </section>
+  )
+}
