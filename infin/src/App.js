be431396d490945733623ce326ced9db9c875988
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-06-29 22:22:47 -0400

    changed  logo/thanos_sec. to HD

--- infin/src/App.js
+++ infin/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header>
         <div>
           <Link to="/">
-            <img id="logo" src="https://vignette.wikia.nocookie.net/logocomics/images/d/d5/Infinity_Gauntlet_%281991%29.png/revision/latest?cb=20150519180606" alt="inflogo" />
+            <img id="logo" src="http://oi68.tinypic.com/2hsahbm.jpg" alt="inflogo" />
           </Link>
           <nav>
             <a href="/#hero-pics">Heroes</a>
