517afc87bc3d8c4ea789ff683959dfcbf7af4e13
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-07-01 16:19:45 -0400

    search page functionality

--- infin/src/components/Search.jsx
+++ infin/src/components/Search.jsx
@@ -4,10 +4,15 @@ import axios from 'axios';
 const apiKey = '10214393741373662';
 
 
-
 class Search extends React.Component {
-  state = {
-    name: null
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      image: '',
+      nickName: '',
+      pwrStats: [],
+    }
   }
   getHero = async (e) => {
     e.preventDefault();
@@ -15,9 +20,15 @@ class Search extends React.Component {
     await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${apiKey}/search/${charName}`)
       .then((res) => {
         // console.log(res.data.results["0"].name);
-        const name = res.data.results["0"].biography;
-        console.log(name);
+        const name = res.data.results["0"].name;
+        const image = res.data.results["0"].image;
+        const nickName = res.data.results["0"].biography["full-name"];
+        const pwrStats = res.data.results["0"].powerstats;
+        // console.log(name);
         this.setState({ name: name })
+        this.setState({ image: image.url })
+        this.setState({ nickName: nickName })
+        this.setState({ pwrStats: pwrStats.arr })
       }).catch(err => {
 
       })
@@ -27,13 +38,24 @@ class Search extends React.Component {
   render() {
 
     return (
-      <Form getHero={this.getHero} />
-      // <div>
-      //   <p>The name is {this.state.name()}</p>
-      // </div>
+      <div>
+        <Form getHero={this.getHero} />
+        <div className="charname">
+          <p>Name:{this.state.name}</p>
+        </div>
+        <img src={this.state.image} />
+        <p>{this.state.nickName}</p>
+        <p>{this.state.pwrStats}</p>
+      </div>
     );
   }
 }
 
 export default Search;
+
+
+
+
+
+
 
