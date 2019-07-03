ee6e05a70fbf1743d0eb9e3a8b8c41bc9448f613
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-07-03 04:39:24 -0400

    trilogy section more into

--- infin/src/components/Trilogy.jsx
+++ infin/src/components/Trilogy.jsx
@@ -5,10 +5,18 @@ import React from 'react';
 export default function Trilogy() {
   return (
     <section id="trilogy">
-      <div id="trilogy-info">
+      <div id="trilogy-img">
         <img src={require('./images/trilogy_section.jpg')} />
-        <h1>testing</h1>
       </div>
-    </section>
+      <div id="trilogy-info">
+        <h1>Cosmic Event</h1>
+        <h2>Infinity Trilogy</h2>
+        <p>All of Marvel's top heroes star in this cosmic epic, where they must stop the Infinity-fueled "Mad Titan"
+          Thanos, Magus (Adam Warlock's evil counterpart), and the Goddess (Adam Warlock's 'benevolent' coutnerpart)
+          - all of whom have been infused with near-omnipotent power thans to the Infinity Gems!</p>
+
+      </div>
+
+    </section >
   )
 };
