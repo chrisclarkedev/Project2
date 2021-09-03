32b7ec3ce119f0436d3e6cb39fbe2f3b5a50cbf2
chrisclarkedev <cclarke8904@gmail.com>
2021-09-03 12:28:20 -0400

    fixed api call

--- infin/src/components/ComicSection.jsx
+++ infin/src/components/ComicSection.jsx
@@ -10,7 +10,13 @@ export default function ComicSection() {
           <img src={require('./images/img1.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #1</h4>
-            <p>Seeking to impress the Mistress Death, who returned him to life, Thanos has gained control over the Infinity Gauntlet -- making him an all-powerful god! With Mephisto as his familiar, Thanos wipes out half of the life in the universe with a snap of his fingers! What will Earth do when half its the heroes are killed?</p>
+            <p>
+              Seeking to impress the Mistress Death, who returned him to life,
+              Thanos has gained control over the Infinity Gauntlet -- making him
+              an all-powerful god! With Mephisto as his familiar, Thanos wipes
+              out half of the life in the universe with a snap of his fingers!
+              What will Earth do when half its the heroes are killed?
+            </p>
           </div>
         </div>
       </div>
@@ -19,7 +25,13 @@ export default function ComicSection() {
           <img src={require('./images/img2.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #2</h4>
-            <p>Billions of people across the planet have disappeared; now, the super humans of Earth scramble to find out what's going on! Featuring Quasar! Dr. Strange! Dr. Doom! Captain America! Thor! Warlock! Wolverine! Iron Man! She-Hulk! The Silver Surfer! And more!</p>
+            <p>
+              Billions of people across the planet have disappeared; now, the
+              super humans of Earth scramble to find out what's going on!
+              Featuring Quasar! Dr. Strange! Dr. Doom! Captain America! Thor!
+              Warlock! Wolverine! Iron Man! She-Hulk! The Silver Surfer! And
+              more!
+            </p>
           </div>
         </div>
       </div>
@@ -28,7 +40,13 @@ export default function ComicSection() {
           <img src={require('./images/img3.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #3</h4>
-            <p>To start the day, Thanos wrote his name across the universe in planets. To finish, he'll stand against the assembled might of Earth's remaining heroes! Plus: When Mistress Death rejects Thanos again, he creates his own perfect mate: Meet Terraxia the Terrible!</p>
+            <p>
+              To start the day, Thanos wrote his name across the universe in
+              planets. To finish, he'll stand against the assembled might of
+              Earth's remaining heroes! Plus: When Mistress Death rejects Thanos
+              again, he creates his own perfect mate: Meet Terraxia the
+              Terrible!
+            </p>
           </div>
         </div>
       </div>
@@ -37,7 +55,13 @@ export default function ComicSection() {
           <img src={require('./images/img4.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #4</h4>
-            <p>Wolverine, broken! The Scarlet Witch, incinerated! She-Hulk and Namor, buried alive! The Vision, ripped apart! Thor, shattered! Iron Man, beheaded! Cyclops, suffocated! When Earth's heroes attack, Thanos takes the kid gloves off -- but leaves the god-making Infinity Gauntlet on!</p>
+            <p>
+              Wolverine, broken! The Scarlet Witch, incinerated! She-Hulk and
+              Namor, buried alive! The Vision, ripped apart! Thor, shattered!
+              Iron Man, beheaded! Cyclops, suffocated! When Earth's heroes
+              attack, Thanos takes the kid gloves off -- but leaves the
+              god-making Infinity Gauntlet on!
+            </p>
           </div>
         </div>
       </div>
@@ -46,7 +70,13 @@ export default function ComicSection() {
           <img src={require('./images/img5.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #5</h4>
-            <p>Galactus! The Celestials! The Stranger! Master Order and Lord Chaos! The all-powerful entities of the universe have united to stand against Thanos -- and they don't stand a chance! Can the remnants of Earth's heroes succeed where the gods have failed? And the captive Nebula has her eyes on that Gauntlet...</p>
+            <p>
+              Galactus! The Celestials! The Stranger! Master Order and Lord
+              Chaos! The all-powerful entities of the universe have united to
+              stand against Thanos -- and they don't stand a chance! Can the
+              remnants of Earth's heroes succeed where the gods have failed? And
+              the captive Nebula has her eyes on that Gauntlet...
+            </p>
           </div>
         </div>
       </div>
@@ -55,11 +85,15 @@ export default function ComicSection() {
           <img src={require('./images/img6.jpg')} />
           <div class="content">
             <h4>Infinity Gauntlet Vol 1 #6</h4>
-            <p>Nebula has the Infinity Gauntlet -- and, in the most shocking twist yet, Thanos now stands with Earth's heroes to defeat her! Can one being possibly contain all that power? And if not Thanos, then who? With a shock ending that'll leave you begging for more!</p>
+            <p>
+              Nebula has the Infinity Gauntlet -- and, in the most shocking
+              twist yet, Thanos now stands with Earth's heroes to defeat her!
+              Can one being possibly contain all that power? And if not Thanos,
+              then who? With a shock ending that'll leave you begging for more!
+            </p>
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
--- infin/src/components/Search.jsx
+++ infin/src/components/Search.jsx
@@ -3,8 +3,6 @@ import Form from './Form';
 import axios from 'axios';
 const apiKey = '10214393741373662';
 
-
-
 class Search extends React.Component {
   constructor() {
     super();
@@ -16,36 +14,38 @@ class Search extends React.Component {
       weight: '',
       appearance: '',
       group: '',
-    }
+    };
   }
   getHero = async (e) => {
     e.preventDefault();
     const charName = e.target.elements.charName.value;
-    await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${apiKey}/search/${charName}`)
+    // await axios
+    //   .get(
+    //     `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${apiKey}/search/${charName}`
+    //   )
+    await axios
+      .get(`https://superheroapi.com/api.php/${apiKey}/search/${charName}`)
       .then((res) => {
-        const name = res.data.results["0"].name;
-        const realName = res.data.results["0"].biography["full-name"];
-        const image = res.data.results["0"].image;
-        const height = res.data.results["0"].appearance.height["0"];
-        const weight = res.data.results["0"].appearance.weight["0"];
-        const appearance = res.data.results["0"].biography["first-appearance"];
-        const group = res.data.results["0"].connections["group-affiliation"];
+        const name = res.data.results['0'].name;
+        const realName = res.data.results['0'].biography['full-name'];
+        const image = res.data.results['0'].image;
+        const height = res.data.results['0'].appearance.height['0'];
+        const weight = res.data.results['0'].appearance.weight['0'];
+        const appearance = res.data.results['0'].biography['first-appearance'];
+        const group = res.data.results['0'].connections['group-affiliation'];
 
-        this.setState({ image: image.url })
-        this.setState({ name: name })
-        this.setState({ realName: realName })
-        this.setState({ height: height })
-        this.setState({ weight: weight })
-        this.setState({ appearance: appearance })
-        this.setState({ group: group })
-      }).catch(err => {
-
+        this.setState({ image: image.url });
+        this.setState({ name: name });
+        this.setState({ realName: realName });
+        this.setState({ height: height });
+        this.setState({ weight: weight });
+        this.setState({ appearance: appearance });
+        this.setState({ group: group });
       })
-
-  }
+      .catch((err) => {});
+  };
 
   render() {
-
     return (
       <div>
         <Form getHero={this.getHero} />
@@ -56,7 +56,10 @@ class Search extends React.Component {
           </div>
           <h2>{this.state.name}</h2>
           <h3>Real Name: {this.state.realName}</h3>
-          <h4>Height: {this.state.height} &nbsp;  &nbsp; &nbsp;Weight: {this.state.weight}</h4>
+          <h4>
+            Height: {this.state.height} &nbsp; &nbsp; &nbsp;Weight:{' '}
+            {this.state.weight}
+          </h4>
           <h5>First Apperance: {this.state.appearance}</h5>
           <h6>Group Affiliaion: {this.state.group}</h6>
         </div>
@@ -66,10 +69,3 @@ class Search extends React.Component {
 }
 
 export default Search;
-
-
-
-
-
-
-
