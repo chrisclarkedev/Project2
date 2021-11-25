99b0932599ec62f411fbed4fe7b697f321e4d31d
chrisclarkedev <cclarke8904@gmail.com>
2021-11-24 19:29:02 -0500

    changes to index.css to align items to center

--- infin/src/components/PicSection.jsx
+++ infin/src/components/PicSection.jsx
@@ -1,29 +1,66 @@
 import React from 'react';
 
-
 export default function ThanosSection() {
   return (
     <section id="pic-section">
       <div id="narrcomic">
-        <h2>The fated Heroes sent to stop "The Mad Titan" Thanos from destroying half of all life in the universe!</h2>
+        <h2>
+          The fated Heroes sent to stop "The Mad Titan" Thanos from destroying
+          half of all life in the universe!
+        </h2>
       </div>
       <br></br>
+
       <div id="hero-gallery">
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/274.jpg" alt="capt-amer" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/140.jpg" alt="thor" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/133.jpg" alt="spdr-man" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/444.jpg" alt="scarl-wit" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/55.jpg" alt="dr_strange" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/83.jpg" alt="hulk" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/154.jpg" alt="adam-warlock" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/161.jpg" alt="wolv" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/85.jpg" alt="iron-man" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/137.jpg" alt="namor" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/125.jpg" alt="she-hulk" />
-        <img src="https://www.superherodb.com/pictures2/portraits/10/100/127.jpg" alt="silvsurf" />
-
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/274.jpg"
+          alt="capt-amer"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/140.jpg"
+          alt="thor"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/133.jpg"
+          alt="spdr-man"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/444.jpg"
+          alt="scarl-wit"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/55.jpg"
+          alt="dr_strange"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"
+          alt="hulk"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/154.jpg"
+          alt="adam-warlock"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/161.jpg"
+          alt="wolv"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/85.jpg"
+          alt="iron-man"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/137.jpg"
+          alt="namor"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/125.jpg"
+          alt="she-hulk"
+        />
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/127.jpg"
+          alt="silvsurf"
+        />
       </div>
-
     </section>
-  )
+  );
 }
