7aef71860b9d17189b713b844140fd6cbf3ac046
chrisclarkedev <cclarke8904@gmail.com>
2021-11-30 15:58:16 -0500

    react boostrap installed

--- infin/src/App.js
+++ infin/src/App.js
@@ -1,42 +1,43 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import './App2.css';
 import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Search from './components/Search';
 import Trilogy from './components/Trilogy';
+import Hero from './components/svg/mask-solid.svg';
+import Logo from './components/images/logo_hd.png';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div>
+        <div className="logo">
           <Link to="/">
-            <img
-              id="logo"
-              src="https://i.ibb.co/ZB2XVk9/logo-hd.png"
-              alt="inflogo"
-            />
+            <img src={Logo} alt="infin saga" />
           </Link>
         </div>
-        <nav>
-          <ul className="nav_links">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="/#pic-section">Heroes</a>
-            </li>
-            <li>
-              <Link to="/trilogy">Trilogy </Link>
-            </li>
-            <li>
-              <Link to="/search">
-                <button>Search Hero</button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </header>
+      <nav>
+        <ul className="nav_links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="/#pic-section">Heroes</a>
+          </li>
+          <li>
+            <Link to="/trilogy">Trilogy</Link>
+          </li>
+          <li>
+            <div className="hero">
+              <Link to="/search">
+                <img src={Hero} alt="" width="40" />
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </nav>
       <main>
         <Route path="/" exact render={() => <Home />} />
         <Route path="/search" render={() => <Search />} />
--- infin/src/components/ThanosSection.jsx
+++ infin/src/components/ThanosSection.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Thanosimg from './images/thanos_section.png';
 import { Link } from 'react-router-dom';
 
 export default function ThanosSection() {
   return (
-    <section id="thanos-section">
-      <div id="thanos-image">
-        <img src={require('./images/thanos_section.png')} />
-      </div>
-    </section>
-  )
+    <div className="thanos-section">
+      <img src={Thanosimg} alt="" />
+    </div>
+  );
 }
